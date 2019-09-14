6c79bda60559e80e2b2cc3d61de14db986157d0a
Adam Weissman <adam@comedytragedyepic.com>
2019-09-14 10:38:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
   reversed = []
-  new_sentence.each do |word|
+  new_sentence.collect do |word|
     reversed << word.reverse
   end
   return reversed.join(" ")
